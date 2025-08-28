84d3e90e03ee00ed0cb7b5d412ebff5762b77db1
Luiz Rogério <luizrogeriocn@gmail.com>
2025-08-27 22:42:59 -0300

    Create MatchLog entity
    This creates our first entity, the MatchLog. It represents the game log
    file received by the system (usually via upload).
    
    In this first iteration, the MatchLog has:
    
    - url: a path pointing to where the actual file is stored
    - status: an enum detailing what is its current status [uploaded |
    processing | processed | failed]
    - processed_at: a timestamp which will be populated once the log is
    processed
    
    Also, an initial configuration was made to connect to a PostgreSQL
    database.

---
+++ src/match-logs/match-log.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum MatchLogStatus {
+  Uploaded = 'uploaded',
+  Processing = 'processing',
+  Processed = 'processed',
+  Failed = 'failed',
+}
+
+@Entity({ name: 'match_logs' })
+export class MatchLog {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  // where the file is stored
+  @Column({ type: 'text' })
+  url: string;
+
+  @Column({
+    type: 'enum',
+    enum: MatchLogStatus,
+    default: MatchLogStatus.Uploaded,
+  })
+  status: MatchLogStatus;
+
+  // should only be nullable until status is either processed or failed
+  @Column({ type: 'timestamptz', nullable: true })
+  processedAt: Date | null;
+
+  @CreateDateColumn({ type: 'timestamptz', name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', name: 'updated_at' })
+  updatedAt: Date;
+}
