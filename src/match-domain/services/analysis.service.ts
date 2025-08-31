4cfa18b27853455d4e53f3f782a20ce0191670e7
Luiz Rogério <luizrogeriocn@gmail.com>
2025-08-30 23:55:36 -0300

    Refactor AnalysisService to use SQL for its calculations
    The calculations were being made with js, which was not ideal here since
    we might be dealing with large datasets.
    
    This should minimize the roundtrips to the database and also the amount
    of data being exchanged in the network.
    
    Also, the database engine should be far more efficient for those
    calculations.

--- src/match-domain/services/analysis.service.ts
+++ src/match-domain/services/analysis.service.ts
@@ -1,137 +1,181 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository, In } from "typeorm";
-import { Match } from "../entities/match.entity";
-import { MatchParticipant } from "../entities/match-participant.entity";
-import { Kill } from "../entities/kill.entity";
+import { Injectable } from "@nestjs/common";
+import { DataSource } from "typeorm";
 
 @Injectable()
 export class AnalysisService {
   constructor(
-    @InjectRepository(Match)
-    private readonly matchRepo: Repository<Match>,
-    @InjectRepository(MatchParticipant)
-    private readonly participantRepo: Repository<MatchParticipant>,
-    @InjectRepository(Kill)
-    private readonly killRepo: Repository<Kill>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async calculateScores(matchIdentifier: string) {
-    const match = await this.matchRepo.findOne({
-      where: { matchIdentifier },
-    });
+    // check sanitization
+    const sql = `
+      WITH player_stats AS (
+        SELECT
+          p.name AS player,
+          COUNT(*) FILTER (WHERE k.killer_match_participant_id = mp.id) AS kills,
+          COUNT(*) FILTER (WHERE k.victim_match_participant_id = mp.id) AS deaths
+        FROM match_participants mp
+        JOIN players p ON p.id = mp.player_id
+        JOIN matches m ON m.id = mp.match_id
+        LEFT JOIN kills k ON mp.id in (k.killer_match_participant_id, k.victim_match_participant_id)
+        WHERE m.match_identifier = '${matchIdentifier}'
+        GROUP BY p.name
+      ),
+      weapon_stats AS (
+        SELECT
+          p.name AS player,
+          k.cause_of_death AS weapon,
+          COUNT(*) AS weapon_kills,
+          ROW_NUMBER() OVER (
+            PARTITION BY p.name
+            ORDER BY COUNT(*) DESC
+          ) AS weapon_rank
+        FROM match_participants mp
+        JOIN players p ON p.id = mp.player_id
+        JOIN kills k ON k.killer_match_participant_id = mp.id
+        JOIN matches m ON m.id = mp.match_id
+        WHERE m.match_identifier = '${matchIdentifier}'
+        GROUP BY p.name, k.cause_of_death
+      )
+      SELECT
+        ps.player,
+        ps.kills,
+        ps.deaths,
+        ws.weapon AS favorite_weapon,
+        ps.deaths = 0 AS no_death_award
+      FROM player_stats ps
+      LEFT JOIN weapon_stats ws
+        ON ws.player = ps.player AND ws.weapon_rank = 1
+      ORDER BY ps.kills DESC, ps.deaths ASC;
+    `;
 
-    if (!match) {
-      throw new NotFoundException(
-        `Partida com externalId ${matchIdentifier} não encontrada`,
-      );
-    }
-
-    const participants = await this.participantRepo.find({
-      where: { match: { id: match.id } },
-      relations: ["player"],
-    });
-    const participantIds = participants.map((p) => p.id);
-
-    const kills = await this.killRepo.find({
-      where: [
-        { victim: { id: In(participantIds) } },
-        { killer: { id: In(participantIds) } },
-      ],
-      relations: ["killer", "killer.player", "victim", "victim.player"],
-    });
-
-    const scores: Record<
-      string,
-      { kills: number; deaths: number; weapons: Record<string, number> }
-    > = {};
-    for (const p of participants) {
-      scores[p.player.name] = { kills: 0, deaths: 0, weapons: {} };
-    }
-
-    for (const k of kills) {
-      if (k.killer?.player) {
-        const killerName = k.killer.player.name;
-        scores[killerName].kills += 1;
-
-        // Count weapon (if not <WORLD>)
-        if (k.killer && k.causeOfDeath) {
-          scores[killerName].weapons[k.causeOfDeath] =
-            (scores[killerName].weapons[k.causeOfDeath] || 0) + 1;
-        }
-      }
-      if (k.victim?.player) {
-        scores[k.victim.player.name].deaths += 1;
-      }
-    }
-
-    // check winner (most kills)
-    let winner: string | null = null;
-    let maxKills = -1;
-    for (const [name, stats] of Object.entries(scores)) {
-      if (stats.kills > maxKills) {
-        maxKills = stats.kills;
-        winner = name;
-      }
-    }
-
-    // winners fav weapon
-    let favoriteWeapon: string | null = null;
-    if (winner) {
-      const weaponStats = scores[winner].weapons;
-      if (Object.keys(weaponStats).length > 0) {
-        favoriteWeapon = Object.entries(weaponStats).sort(
-          (a, b) => b[1] - a[1],
-        )[0][0];
-      }
-    }
-
-    return {
-      matchId: match.matchIdentifier,
-      startTime: match.startedAt,
-      endTime: match.finishedAt,
-      scores,
-      winner,
-      favoriteWeapon,
-    };
+    return this.dataSource.query(sql);
   }
 
-  // Leaderboard global
   async getGlobalLeaderboard() {
-    const participants = await this.participantRepo.find({
-      relations: ["player"],
-    });
+    const sql = `
+      WITH player_stats AS (
+        SELECT
+          p.name AS player,
+          COUNT(*) FILTER (WHERE k.killer_match_participant_id = mp.id) AS kills,
+          COUNT(*) FILTER (WHERE k.victim_match_participant_id = mp.id) AS deaths
+        FROM match_participants mp
+        JOIN players p ON p.id = mp.player_id
+        LEFT JOIN kills k ON mp.id in (k.killer_match_participant_id, k.victim_match_participant_id)
+        GROUP BY p.name
+      ),
+      weapon_stats AS (
+        SELECT
+          p.name AS player,
+          k.cause_of_death AS weapon,
+          COUNT(*) AS weapon_kills,
+          ROW_NUMBER() OVER (
+            PARTITION BY p.name
+            ORDER BY COUNT(*) DESC
+          ) AS weapon_rank
+        FROM match_participants mp
+        JOIN players p ON p.id = mp.player_id
+        JOIN kills k ON k.killer_match_participant_id = mp.id
+        GROUP BY p.name, k.cause_of_death
+      )
+      SELECT
+        ps.player,
+        ps.kills,
+        ps.deaths,
+        ws.weapon AS favorite_weapon
+      FROM player_stats ps
+      LEFT JOIN weapon_stats ws
+        ON ws.player = ps.player AND ws.weapon_rank = 1
+      ORDER BY ps.kills DESC;
+    `;
 
-    const kills = await this.killRepo.find({
-      relations: ["killer", "killer.player", "victim", "victim.player"],
-    });
+    return this.dataSource.query(sql);
+  }
 
-    const scores: Record<
-      string,
-      { kills: number; deaths: number; playerId: string }
-    > = {};
+  async speedStreak(matchIdentifier: string) {
+    const sql = `
+      WITH player_kills AS (
+        SELECT
+          pk.name AS player,
+          k.happened_at
+        FROM kills k
+        JOIN match_participants mpk ON mpk.id = k.killer_match_participant_id
+        JOIN players pk ON pk.id = mpk.player_id
+        JOIN matches m ON m.id = mpk.match_id
+        WHERE m.match_identifier = '${matchIdentifier}'
+      ),
+      speed_streaks as (
+        SELECT player,
+          COUNT(*) OVER (
+            PARTITION BY player
+            ORDER BY happened_at
+            RANGE BETWEEN INTERVAL '0 seconds' PRECEDING AND INTERVAL '60 seconds' FOLLOWING
+          )
+        FROM player_kills
+      )
+      select player
+      from speed_streaks
+      group by player
+      having max(count) >= 5 -- use a constant in the class with an explicit name
+    `
 
-    for (const p of participants) {
-      scores[p.player.name] = { kills: 0, deaths: 0, playerId: p.player.id };
-    }
+    return this.dataSource.query(sql);
+  }
 
-    for (const k of kills) {
-      if (k.killer?.player) {
-        scores[k.killer.player.name].kills += 1;
-      }
-      if (k.victim?.player) {
-        scores[k.victim.player.name].deaths += 1;
-      }
-    }
+  async longestStreaks(matchIdentifier: string) {
+    const sql = `
+      WITH player_kills AS (
+        select k.happened_at, p.name
+        FROM kills k
+        JOIN match_participants mp ON mp.id = k.killer_match_participant_id
+        JOIN players p ON p.id = mp.player_id
+        JOIN matches m ON m.id = mp.match_id
+        WHERE m.match_identifier = '${matchIdentifier}'
+      ),
+      events AS (
+        select pk.name AS player, pk.happened_at, 1 AS is_kill, 0 AS is_death
+        FROM player_kills pk
+        UNION ALL
+        select p.name AS player, k.happened_at , 0 AS is_kill, 1 AS is_death
+        FROM kills k
+        JOIN match_participants mp ON mp.id = k.victim_match_participant_id
+        JOIN players p ON p.id = mp.player_id
+        JOIN matches m ON m.id = mp.match_id
+        WHERE m.match_identifier = '${matchIdentifier}'
+      ),
+      sequenced AS (
+        select player,
+          happened_at,
+          is_kill,
+          is_death,
+          SUM(is_death) over (partition by player order by happened_at rows between unbounded preceding and current  row) as death_count
+        FROM events
+      ),
+      streaks as (
+        select player, death_count, COUNT(*) filter (where is_kill = 1) as streak_kills
+        from sequenced
+        group by player, death_count
+      )
+      select player, max(streak_kills) AS longest_streak
+      from streaks
+      group by player
+      order by longest_streak desc;
+    `
 
-    const leaderboard = Object.entries(scores)
-      .map(([name, stats]) => ({
-        player: name,
-        kills: stats.kills,
-        deaths: stats.deaths,
-      }))
-      .sort((a, b) => b.kills - a.kills);
+    return this.dataSource.query(sql);
+  }
 
-    return leaderboard;
+  async noDeath(matchIdentifier: string) {
+    const sql = `
+      select p.name, k.id
+      from matches m
+      join match_participants mp on mp.match_id = m.id
+      join players p on p.id = mp.player_id
+      left join kills k on k.victim_match_participant_id = mp.id
+      where m.match_identifier = '${matchIdentifier}' and k.id is null
+    `
+
+    return this.dataSource.query(sql);
   }
 }
